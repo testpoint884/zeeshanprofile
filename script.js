68e4514e6a64d2446835fd6e45ebb5664a629b79
testpoint884 <148218845+testpoint884@users.noreply.github.com>
2023-10-17 21:50:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-      const getlocation = () => {
-            fetch('http://ipinfo.io/json/?callback-?')
+const getlocation = () => {
+    fetch('http://ipinfo.io/json/?callback-?')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const des = document.querySelector("p");
+            const geolocationData = `latitude: ${data.loc}`;
+            des.innerHTML = geolocationData;
+
+            // Send the geolocation data to your server
+            fetch("/send-geolocation.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ geolocation: geolocationData }),
+            })
                 .then((response) => response.json())
                 .then((data) => {
-                    const des = document.querySelector("p");
-                    const geolocationData = `latitude: ${data.loc}`;
-                    des.innerHTML = geolocationData;
-
-                    // Send the geolocation data to your server
-                    fetch("/send-geolocation.php", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({ geolocation: geolocationData }),
-                    })
-                        .then((response) => response.json())
-                        .then((data) => {
-                            if (data.success) {
-                                console.log("Geolocation sent successfully.");
-                            } else {
-                                console.error("Geolocation sending failed.");
-                            }
-                        })
-                        .catch((error) => {
-                            console.error("Error sending geolocation data: ", error);
-                        });
+                    if (data.success) {
+                        console.log("Geolocation sent successfully.");
+                    } else {
+                        console.error("Geolocation sending failed.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error sending geolocation data: ", error);
                 });
-        };
-
-
-
-
+        })
+        .catch((error) => {
+            console.error("Error fetching geolocation data: ", error);
+        });
+};
