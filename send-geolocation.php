a2f1e69ea43fce12ee99778976654d16c10c1943
testpoint884 <148218845+testpoint884@users.noreply.github.com>
2023-10-18 00:01:06 +0500

    Update send-geolocation.php

--- send-geolocation.php
+++ send-geolocation.php
@@ -1,35 +1,12 @@
 <?php
-header("Content-Type: application/json");
 
-// Get the POST data containing geolocation information
-$postData = file_get_contents("php://input");
-$data = json_decode($postData);
 
-if ($data !== null) {
-    // Extract the geolocation data
-    $geolocationData = $data->geolocation;
+$to = "your-email@gmail.com"; // Replace with your Gmail address
+$subject = "Geolocation Data";
+$message = "Latitude: $latitude, Longitude: $longitude";
+$headers = "From: your-email@gmail.com"; // Replace with your Gmail address
 
-    // Replace with your email address
-    $to = "aslamrehan884@gmail.com";
+$success = mail($to, $subject, $message, $headers);
 
-    // Subject for the email
-    $subject = "Geolocation Data";
-
-    // Compose the email message
-    $message = "Geolocation data received:\n" . $geolocationData;
-
-    // Additional email headers
-    $headers = "From: muhammadrehan323232@gmail.com"; // Replace with your email address
-
-    // Send the email
-    $success = mail($to, $subject, $message, $headers);
-
-    if ($success) {
-        echo json_encode(["success" => true]);
-    } else {
-        echo json_encode(["success" => false]);
-    }
-} else {
-    echo json_encode(["success" => false, "error" => "Invalid data received"]);
-}
+echo json_encode(["success" => $success]);
 ?>
