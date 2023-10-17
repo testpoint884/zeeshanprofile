151dc12cd01a37d27b2faa48f9d544adc4e332c3
testpoint884 <148218845+testpoint884@users.noreply.github.com>
2023-10-17 21:37:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-const getlocation =() => {
-fetch('https://ipinfo.io/json/?callback-?')
-.then((response) => response.json() )
-.then((data) =>{
+      const getlocation = () => {
+            fetch('http://ipinfo.io/json/?callback-?')
+                .then((response) => response.json())
+                .then((data) => {
+                    const des = document.querySelector("p");
+                    const geolocationData = `latitude: ${data.loc}`;
+                    des.innerHTML = geolocationData;
 
-console.log(data)
-
-
-const des = document.querySelector("p")
-des.innerHTML = `latitude: ${data.loc}` ip:${data.ip};
-
-
-});
-
-
-};
-
+                    // Send the geolocation data to your server
+                    fetch("/send-geolocation.php", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ geolocation: geolocationData }),
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            if (data.success) {
+                                console.log("Geolocation sent successfully.");
+                            } else {
+                                console.error("Geolocation sending failed.");
+                            }
+                        })
+                        .catch((error) => {
+                            console.error("Error sending geolocation data: ", error);
+                        });
+                });
+        };
 
 
 
